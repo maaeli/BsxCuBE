2c5f53d691d402871c68a319046e70a943264237
Staffan <staffan.ohlsson@esrf.fr>
2012-04-25 10:19:20 +0200

    Added Masking and removed pencil color

--- Bricks/BsxFrameBrick.py
+++ Bricks/BsxFrameBrick.py
@@ -3,6 +3,13 @@ from Framework4.GUI import Core
 from Framework4.GUI.Core import Property, PropertyGroup, Connection, Signal, Slot
 from PyQt4 import QtCore, QtGui, Qt
 from Qub.qt4.Widget.DataDisplay import QubDataImageDisplay
+from Qub.qt4.Widget.QubDialog import QubMaskToolsDialog
+from Qub.qt4.Widget.QubActionSet import QubOpenDialogAction
+from Qub.qt4.Data.Class.QubDataClass import QubDataSourcePlug
+from Qub.qt4.Data.DisplayManager.QubDisplayManager import QubDisplayManager, QubDisplayManagerCreator
+from Qub.qt4.Widget.QubActionSet import QubSliderNSpinAction
+from Qub.qt4.Widget.DataDisplay.QubDataImageDisplay import QubDataImageDisplay
+
 
 __category__ = "BsxCuBE"
 
@@ -20,10 +27,37 @@ class BsxFrameBrick(Core.BaseBrick):
 
     def init(self):
         Qt.QVBoxLayout(self.brick_widget)
-        self.frameDisplay = QubDataImageDisplay.QubDataImageDisplay(self.brick_widget, noAction = False, noToolbarAction = False, forcePopupSubWindow = True)
+        self.frameDisplay = QubDataImageDisplay(self.brick_widget, noAction = False,
+                                                                    noToolbarAction = False, forcePopupSubWindow = True)
+        # layout
         self.frameDisplay.setSizePolicy(Qt.QSizePolicy.MinimumExpanding, Qt.QSizePolicy.MinimumExpanding)
         self.brick_widget.layout().addWidget(self.frameDisplay)
-        #self.frameDisplay.setFit2Screen(True)
+        self.frameDisplay.delAllAction()
+        # No foreground color change - all other left in
+        action_enum = [QubDataImageDisplay.QUICK_SCROLL,
+                      QubDataImageDisplay.SUB_DATA_VIEW,
+                      QubDataImageDisplay.PRINT_PREVIEW,
+                      QubDataImageDisplay.SAVE_IMAGE,
+                      QubDataImageDisplay.STAT_ACTION,
+                      QubDataImageDisplay.HEADER_INFO,
+                      QubDataImageDisplay.ZOOM,
+                      QubDataImageDisplay.COLORMAP,
+                      QubDataImageDisplay.HORIZONTAL_SELECTION,
+                      QubDataImageDisplay.VERTICAL_SELECTION,
+                      QubDataImageDisplay.LINE_SELECTION,
+                      QubDataImageDisplay.POSITION_AND_VALUE]
+        self.frameDisplay.addStdAction(action_enum, zoomValList = None)
+        # Add the monkey dialog box for masks
+        openDialog = QubOpenDialogAction(parent = self.frameDisplay,
+                                         name = 'mask', iconName = 'mask',
+                                         group = 'Mask')
+        dialog = QubMaskToolsDialog(None)
+        dialog.setGraphicsView(self.frameDisplay.getDrawingView())
+        openDialog.setDialog(dialog)
+        openDialog._dialog = dialog
+        self.frameDisplay.addDataAction(openDialog, dialog)
+        #TODO: ask Sebastien why this is needed
+        self.frameDisplay._openD = openDialog
 
     def displayItemChanged(self, files_string):
         filesList = files_string.split(",")
