2514506e6ae47247b52f0d58b8d9e5cc644c6645
Staffan <staffan.ohlsson@esrf.fr>
2012-04-27 07:16:02 +0200

    Now I can finally see something ;-)

--- Bricks/LoginBrick.py
+++ Bricks/LoginBrick.py
@@ -1,20 +1,15 @@
-import logging
-
 from Framework4.GUI      import Core
-from Framework4.GUI.Core import Connection, Signal
-from PyQt4 import Qt
+from Framework4.GUI.Core import Connection, Signal, Slot
+from PyQt4 import QtGui, Qt
+import sip
 
-__category__ = "BsxCuBE"
+__category__ = "General"
 
 class LoginBrick(Core.BaseBrick):
 
     properties = {}
 
-    connections = {"login": Connection("Login object",
-                    [],
-                    [],
-                    "loginConnected")}
-    signals = [Signal("loggedIn")]
+    signals = []
     slots = []
 
 
@@ -22,43 +17,70 @@ class LoginBrick(Core.BaseBrick):
         Core.BaseBrick.__init__(self, *args, **kargs)
 
 
-
     def init(self):
         self.__password = ""
         self.__user = "nobody"
+        self.loginProxy = None
+        # Just To Display (from Attenuator
+        self.__filtersDialog = None
+        self.__maskFormat = ""
+        self.__suffix = ""
+        self.__minimumValue = 0
+        self.__maximumValue = 100
+        self.hBoxLayout = Qt.QHBoxLayout()
 
-        # layout
-        self.modeLabel = Qt.QLabel("USER", self.brick_widget)
-        self.modeLabel.setMinimumWidth(55)
-        self.modeLabel.setToolTip("Current mode")
-        self.modePushButton = Qt.QPushButton("Change", self.brick_widget)
-        self.modePushButton.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding)
-        self.modePushButton.setToolTip("Change to expert mode")
-        Qt.QObject.connect(self.modePushButton, Qt.SIGNAL("clicked()"), self.modePushButtonClicked)
+        self.transmissionLabel = Qt.QLabel("Transmission (current, new)", self.brick_widget)
+        self.hBoxLayout.addWidget(self.transmissionLabel)
 
-    def passwordChanged(self, pValue):
-        self.__password = pValue
+        self.currentTransmissionLineEdit = Qt.QLineEdit(self.brick_widget)
+        self.currentTransmissionLineEdit.setEnabled(False)
+        self.currentTransmissionLineEdit.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding)
+        self.currentTransmissionLineEdit.setToolTip("Current transmission")
+        self.hBoxLayout.addWidget(self.currentTransmissionLineEdit)
 
-    def loginConnected(self, pPeer):
-        # we are connected, let us block the other bricks
-        self.__loggedIn = False
-        self.emit("loggedIn", self.__loggedIn)
+        self.newTransmissionComboBox = Qt.QComboBox(self.brick_widget)
+        self.newTransmissionComboBox.setEditable(True)
+        self.newTransmissionComboBox.lineEdit().setMaxLength(10)
+        self.newTransmissionComboBox.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding)
+        self.newTransmissionComboBox.setToolTip("New transmission")
+        self.hBoxLayout.addWidget(self.newTransmissionComboBox)
 
-    def modePushButtonClicked(self):
-        if self.__expertMode:
-            self.__expertMode = False
-            self.emit("expertModeChanged", self.__expertMode)
-            self.modeLabel.setText("USER")
-            self.modePushButton.setToolTip("Change to expert mode")
-            Qt.QMessageBox.information(self.brick_widget, "Info", "You are in user mode now!")
+        self.filtersPushButton = Qt.QPushButton("Filters", self.brick_widget)
+        self.filtersPushButton.setToolTip("Enable/disable transmission filters")
+
+        if self.brick_widget.layout() is not None:
+            self.hBoxLayout.setParent(None)
+            self.brick_widget.layout().removeWidget(self.filtersPushButton)
+            sip.transferback(self.brick_widget.layout())
+        self.brick_widget.setLayout(Qt.QHBoxLayout())
+        self.filtersPushButton.setSizePolicy(Qt.QSizePolicy.Fixed, Qt.QSizePolicy.Expanding)
+        self.brick_widget.layout().addLayout(self.hBoxLayout)
+        self.brick_widget.layout().addWidget(self.filtersPushButton)
+        self.newTransmissionComboBoxChanged(None)
+
+    # From attenuatorBrick    
+    def newTransmissionComboBoxChanged(self, pValue):
+        if pValue is None or pValue == "":
+            self.newTransmissionComboBox.lineEdit().palette().setColor(QtGui.QPalette.Base, QtGui.QColor(255, 255, 0))
         else:
-            password, buttonOk = Qt.QInputDialog.getText(self.brick_widget, "Login", "\nPlease, insert password for expert mode:", Qt.QLineEdit.Password)
-            if buttonOk:
-                if password == self.__password:
-                    self.__expertMode = True
-                    self.emit("expertModeChanged", self.__expertMode)
-                    self.modeLabel.setText("EXPERT")
-                    self.modePushButton.setToolTip("Change to user mode")
-                    Qt.QMessageBox.information(self.brick_widget, "Info", "You are in expert mode now!")
-                else:
-                    Qt.QMessageBox.critical(self.brick_widget, "Error", "Invalid password. Please, try again!")
+            if self.newTransmissionComboBox.lineEdit().hasAcceptableInput():
+                self.newTransmissionComboBox.lineEdit().palette().setColor(QtGui.QPalette.Base, QtGui.QColor(255, 255, 255))
+            else:
+                self.newTransmissionComboBox.lineEdit().palette().setColor(QtGui.QPalette.Base, QtGui.QColor(255, 0, 0))
+        self.newTransmissionComboBox.lineEdit().update()
+
+
+
+#    def passwordChanged(self, pValue):
+#        self.__password = pValue
+#
+#    def loginConnected(self, pPeer):
+#        if pPeer is not None:
+#            # we are connected, let us block the other bricks
+#            self.__loggedIn = False
+#            self.loginProxy = pPeer
+#            self.loginProxy.loginTry("tes")
+#            self.emit("loggedIn", self.__loggedIn)
+#
+#    def modePushButtonClicked(self):
+#        print "Pushed button"
