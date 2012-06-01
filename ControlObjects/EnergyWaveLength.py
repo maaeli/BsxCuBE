11415d8e60d5f238ca3e984135e1c1d698ec9b30
Staffan <staffan.ohlsson@esrf.fr>
2012-06-01 13:48:52 +0200

    In progress: To integratga

--- ControlObjects/EnergyWaveLength.py
+++ ControlObjects/EnergyWaveLength.py
@@ -1,3 +1,4 @@
+import logging
 from Framework4.Control.Core.CObject import CObjectBase, Signal, Slot
 
 
@@ -9,7 +10,29 @@ class EnergyWaveLength(CObjectBase):
 
     def __init__(self, *args, **kwargs):
         CObjectBase.__init__(self, *args, **kwargs)
+        # Threshold in keV (to change the sensitivity)
+        self.__pilatusThreshold = 12.00
+        # get Pilatusdevice from config file
+        # <data name="uri"  value="bm29/pilatus/p0" />
+        self.pilatusDevName = str(self.config["/object/data[@name='uri']/@value"][0])
+        # add a channel to read the Threshold constaqntly
+        self.addChannel('tango',
+                        'pilatus_threshold',
+                        self.pilatusDevName,
+                        'energy_threshold',
+                        polling = 3000)
+        # get threshold constantly
+        self.pilatusThreshold = self.channels.get("pilatus_threshold")
+        if  self.pilatusThreshold is not None:
+            self.connect('update', self.pilatusThresholdChanged)
+        else:
+            logging.error("No connection to Pilatus")
 
+        self.addChannel('tango',
+                        'pilatus_threshold',
+                        self.pilatusDevName,
+                        'energy_threshold',
+                        polling = 3000)
 
 
     def init(self):
@@ -18,11 +41,16 @@ class EnergyWaveLength(CObjectBase):
         # set up connection to Spec motor energy
         # get spec motor as described in href and the corresponding energy.xml
         self.__energyMotor = self.objects["getEnergy"]
-        # connect to the signals from the CO Object specmotor
-        self.__energyMotor.connect("positionChanged", self.newEnergy)
+        if self.__energyMotor is not None:
+            # connect to the signals from the CO Object specmotor
+            self.__energyMotor.connect("positionChanged", self.newEnergy)
+        else:
+            logging.error("No connection to energy motor in spec")
+
 
     def newEnergy(self, pValue):
         self.__energy = float(pValue)
+        self.pilatusThreshold.set_value(self.__energy)
         # Calculate wavelength
         wavelength = self.hcOverE / self.__energy
         wavelengthStr = "%.4f" % wavelength
@@ -36,3 +64,8 @@ class EnergyWaveLength(CObjectBase):
     def getEnergy(self):
         return self.__energyMotor.position()
 
+    def pilatusThresholdChanged(self, pValue):
+        print "New Threshold"
+        self.__pilatusThreshold = pValue
+        print pValue
+        print type(pValue)
