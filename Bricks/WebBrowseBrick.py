be220d979594a444dbf41ab8f5f28ef7ecf9ff96
Staffan <staffan.ohlsson@esrf.fr>
2012-07-03 17:37:35 +0200

    Renamed WebBrowser to WebBroseBrick and all worked... Added IspyB

---
+++ Bricks/WebBrowseBrick.py
@@ -0,0 +1,63 @@
+import logging
+import os
+from Framework4.GUI import Core
+from Framework4.GUI.Core import Property, Connection, Signal, Slot
+from PyQt4 import QtCore, QtGui, Qt
+from PyQt4.QtWebKit import QWebView
+from PyQt4.QtGui import QApplication
+
+from PyQt4.QtCore import QUrl
+
+
+__category__ = "BsxCuBE"
+
+
+
+class WebBrowseBrick(Core.BaseBrick):
+
+    properties = {"url": Property("string", "URL", "", "urlChanged")}
+
+    connections = {"login": Connection("Login object",
+                                        [Signal("loggedIn", "loggedIn")],
+                                        [],
+                                        "connectionToLogin")}
+
+    signals = []
+    slots = []
+
+
+    def __init__(self, *args, **kargs):
+        Core.BaseBrick.__init__(self, *args, **kargs)
+
+    def init(self):
+        self.webViewer = QWebView()
+        self.urlBlank = "about:blank"
+        mainLayout = Qt.QHBoxLayout(self.brick_widget)
+        mainLayout.addWidget(self.webViewer)
+        self.brick_widget.setLayout(mainLayout)
+#        self.brick_widget.setLayout(Qt.QVBoxLayout())
+#        self.brick_widget.layout().setAlignment(QtCore.Qt.AlignTop)
+#        self.brick_widget.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Expanding)
+
+#        self.hBoxLayout0 = Qt.QHBoxLayout()
+#        self.hBoxLayout0.addWidget(self.webViewer)
+#        self.brick_widget.layout().addLayout(self.hBoxLayout0)
+
+
+    def urlChanged(self, url):
+        self.urlConfig = url
+
+   # When connected to Login, then block the brick
+    def connectionToLogin(self, pPeer):
+        if pPeer is not None:
+            self.brick_widget.setEnabled(False)
+            self.webViewer.load(QUrl(self.urlBlank))
+
+    # Logged In : True or False 
+    def loggedIn(self, pValue):
+        self.brick_widget.setEnabled(pValue)
+        if pValue:
+            self.webViewer.load(QUrl(self.urlConfig))
+        else:
+            self.webViewer.load(QUrl(self.urlBlank))
+
