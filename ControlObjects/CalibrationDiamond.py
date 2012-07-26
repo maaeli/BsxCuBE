d6ed1153741b8f1c62fccf0ee33d02f6c5e525ae
Staffan <staffan.ohlsson@esrf.fr>
2012-07-26 13:25:13 +0200

    Cleaned up CalibrationDiamond

--- ControlObjects/CalibrationDiamond.py
+++ ControlObjects/CalibrationDiamond.py
@@ -2,13 +2,11 @@ import logging
 from Framework4.Control.Core.CObject import CObjectBase, Signal, Slot
 
 class CalibrationDiamond(CObjectBase):
-    __CHANNEL_LIST = ["calibrationStatusChanged", "newPositionChanged"]
 
 
-    signals = [Signal(channel) for channel in __CHANNEL_LIST]
+    signals = []
 
-    slots = [Slot("executeCalibration"),
-             Slot("setPosition")]
+    slots = []
 
 
 
@@ -18,32 +16,6 @@ class CalibrationDiamond(CObjectBase):
 
 
     def init(self):
-        for channel in self.__CHANNEL_LIST:
-            try:
-                value = self.channels.get(channel[:-7])
-                setattr(self, channel[:-7], value)
-                if value is not None:
-                    getattr(self, channel[:-7]).connect("update", getattr(self, channel))
-            except Exception, e:
-                logging.getLogger().error("Full Exception: " + str(e))
-
-    def executeCalibration(self, pValue):
-        if pValue == "":
-            self.commands["executeCalibration"]()
-        else:
-            self.commands["executeCalibration"](pValue)
-
-
-    def setPosition(self, pValue):
-        self.commands["setPosition"](pValue)
-
-
-
-    def calibrationStatusChanged(self, pValue):
-        self.emit("calibrationStatusChanged", pValue)
-
-
-    def newPositionChanged(self, pValue):
-        self.emit("newPositionChanged", pValue)
+        pass
 
 
