0d69af08ddcf0f4849c7652b1dceff9b611c2603
Staffan <staffan.ohlsson@esrf.fr>
2012-08-22 16:41:55 +0200

    From Alejandro

--- Bricks/BiosaxsClient.py
+++ Bricks/BiosaxsClient.py
@@ -1,41 +1,37 @@
-from xml.dom.minidom import parse, parseString
 from suds.client import Client
 from suds.transport.http import HttpAuthenticated
-from suds.sax.date import DateTime
-
-
-
 
 class BiosaxsClient:
-	def __init__(self, user, password):
-		self.user = user
-		self.password = password
-		self.httpAuthenticatedToolsForAutoprocessingWebService = HttpAuthenticated(username=self.user, password=self.password)		
-		self.client = Client('http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl', transport = self.httpAuthenticatedToolsForAutoprocessingWebService)
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+        self.httpAuthenticatedToolsForAutoprocessingWebService = HttpAuthenticated(username = self.user, password = self.password)
+        #self.client = Client('http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl', transport = self.httpAuthenticatedToolsForAutoprocessingWebService)
+        self.client = Client('http://160.103.210.4:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl', transport = self.httpAuthenticatedToolsForAutoprocessingWebService)
 
-	def getExperimentsByProposalId(self, id):
-		response = self.client.service.findExperimentByPosposalId(415)
-		experiments = []
-		for experiment in response:
-			experiments.append(Experiment(experiment))
-		return experiments
+    def getExperimentsByProposalId(self, proposalId):
+        response = self.client.service.findExperimentByPosposalId(3092)
+        experiments = []
+        for experiment in response:
+            experiments.append(Experiment(experiment))
+        return experiments
 
-	def getRobotXMLByPlateIds(self, experimentId, ids):
-		textIds = ''
-		for id in ids:
-			textIds = str(id) + " " + textIds
+    def getRobotXMLByPlateIds(self, experimentId, proposalIds):
+        textIds = ''
+        for myId in proposalIds:
+            textIds = str(myId) + " " + textIds
 
-		response = self.client.service.getRobotXMLByPlateIds(experimentId, textIds)
-		return response
+        response = self.client.service.getRobotXMLByPlateIds(experimentId, textIds)
+        return response
 
 
 class Experiment:
-	def __init__(self, experiment):
-		self.experiment = experiment
-		print 'creating experiment'
+    def __init__(self, experiment):
+        self.experiment = experiment
+        print 'creating experiment'
 
-        def getPlates(self):
-		return self.experiment.samplePlate3VOs
+    def getPlates(self):
+        return self.experiment.samplePlate3VOs
 
 
 
