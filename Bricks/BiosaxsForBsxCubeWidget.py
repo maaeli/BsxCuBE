9ce40c3c8ea82572b9843f512caec220be7f91b2
Staffan <staffan.ohlsson@esrf.fr>
2012-09-11 14:08:39 +0200

    Add ons from Alexandre

--- Bricks/BiosaxsForBsxCubeWidget.py
+++ Bricks/BiosaxsForBsxCubeWidget.py
@@ -21,7 +21,7 @@ class Ui_Widget(object):
     def setupUi(self, Widget):
         Widget.setObjectName(_fromUtf8("Widget"))
         Widget.resize(596, 362)
-      
+
         self.response = None
 
         self.label = QtGui.QLabel(Widget)
@@ -31,7 +31,7 @@ class Ui_Widget(object):
         self.open_ISPYB_WUI_Button = QtGui.QPushButton(Widget)
         self.open_ISPYB_WUI_Button.setGeometry(QtCore.QRect(430, 80, 151, 23))
         self.open_ISPYB_WUI_Button.setObjectName(_fromUtf8("open_ISPYB_WUI_Button"))
-	#self.open_ISPYB_WUI_Button.clicked.connect(self.open_ISPyB_WebWidget)
+        #self.open_ISPYB_WUI_Button.clicked.connect(self.open_ISPyB_WebWidget)
 
         self.tableWidget = QtGui.QTableWidget(Widget)
         self.tableWidget.setEnabled(True)
@@ -42,7 +42,7 @@ class Ui_Widget(object):
         self.tableWidget.setRowCount(0)
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
-	QtCore.QObject.connect(self.tableWidget, QtCore.SIGNAL("selectionChanged()"), self.getSelection)
+        QtCore.QObject.connect(self.tableWidget, QtCore.SIGNAL("selectionChanged()"), self.getSelection)
         QtCore.QObject.connect(self.tableWidget, QtCore.SIGNAL(_fromUtf8("itemSelectionChanged()")), self.getSelection)
 
         item = QtGui.QTableWidgetItem()
@@ -60,18 +60,18 @@ class Ui_Widget(object):
         self.Load_plates_button = QtGui.QPushButton(Widget)
         self.Load_plates_button.setGeometry(QtCore.QRect(140, 330, 141, 23))
         self.Load_plates_button.setObjectName(_fromUtf8("Load_plates_button"))
-	self.Load_plates_button.clicked.connect(self.onLoadPlatesButtonClicked)	
-	
+        self.Load_plates_button.clicked.connect(self.onLoadPlatesButtonClicked)
+
         self.comboBox = QtGui.QComboBox(Widget)
         self.comboBox.setGeometry(QtCore.QRect(10, 30, 411, 22))
         self.comboBox.setObjectName(_fromUtf8("comboBox"))
-	QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL("currentIndexChanged(QString)"), self.onExperimentChosen)
+        QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL("currentIndexChanged(QString)"), self.onExperimentChosen)
 
         self.refresh_data_button = QtGui.QPushButton(Widget)
         self.refresh_data_button.setGeometry(QtCore.QRect(430, 30, 151, 23))
         self.refresh_data_button.setObjectName(_fromUtf8("refresh_data_button"))
-	self.refresh_data_button.clicked.connect(self.onRefreshDataButtonClicked)
-      
+        self.refresh_data_button.clicked.connect(self.onRefreshDataButtonClicked)
+
         self.close_button.clicked.connect(self.onCloseButtonClicked)
 
         self.retranslateUi(Widget)
@@ -79,57 +79,57 @@ class Ui_Widget(object):
 
 
     def onLoadPlatesButtonClicked(self):
-	plateIds = self.getSelection()
-	self.response = self.client.getRobotXMLByPlateIds(self.experiment.experiment.experimentId, plateIds)
+        plateIds = self.getSelection()
+        self.response = self.client.getRobotXMLByPlateIds(self.experiment.experiment.experimentId, plateIds)
         self.tableWidget.parentWidget().accept()
 
 
     def loadSamplePlateData(self, data):
-	self.tableWidget.setRowCount(len(data))
-	#for row in data
-		
-    def getSelection(self):
-	print "getSelection() ..."
+        self.tableWidget.setRowCount(len(data))
+        #for row in data
 
-	plateIds = set()
-	for item in self.tableWidget.selectedIndexes():
-		#plateIds.add(item.row())
-		plateIds.add(self.experiment.getPlates()[item.row()].samplePlateId)
-		#print item.row()
-	for id in plateIds:
-		print id
-	return plateIds
-	
+    def getSelection(self):
+        print "getSelection() ..."
+
+        plateIds = set()
+        for item in self.tableWidget.selectedIndexes():
+            #plateIds.add(item.row())
+            plateIds.add(self.experiment.getPlates()[item.row()].samplePlateId)
+        #print item.row()
+        for id in plateIds:
+            print id
+        return plateIds
+
 
     def onRefreshDataButtonClicked(self):
-	#Clean comboBox
-	while (self.comboBox.count() <> 0):
-		self.comboBox.removeItem(0)
+        #Clean comboBox
+        while (self.comboBox.count() <> 0):
+            self.comboBox.removeItem(0)
 
-	self.client = BiosaxsClient('mx415', 'pimx415') 
-	self.experiments = self.client.getExperimentsByProposalId(415)
-	for experiment in self.experiments:
-		self.comboBox.addItem(experiment.experiment.name, experiment.experiment.experimentId)
+        self.client = BiosaxsClient('mx1438', 'Rfo4-73')
+        self.experiments = self.client.getExperimentsByProposalId(3124)
+        for experiment in self.experiments:
+            self.comboBox.addItem(experiment.experiment.name, experiment.experiment.experimentId)
 
 
     def onCloseButtonClicked(self):
         self.tableWidget.parentWidget().reject()
 
     def onExperimentChosen(self):
-	print self.comboBox.currentIndex()
-	print self.experiments[self.comboBox.currentIndex()].experiment.name
-	self.experiment = self.experiments[self.comboBox.currentIndex()]
-	self.tableWidget.setRowCount(len(self.experiments[self.comboBox.currentIndex()].getPlates()))
-	i = 0
-	for  plate in self.experiments[self.comboBox.currentIndex()].getPlates():
-		print plate.name + "  " + plate.platetype3VO.name
-		name=QtGui.QTableWidgetItem(plate.name)
-		sampletype=QtGui.QTableWidgetItem(plate.platetype3VO.name)
-		storageTemperature=QtGui.QTableWidgetItem(plate.storageTemperature)
-                self.tableWidget.setItem(i, 0, name)
-		self.tableWidget.setItem(i, 1, sampletype)
-		self.tableWidget.setItem(i, 2, storageTemperature)
-		i = i + 1		
+        print self.comboBox.currentIndex()
+        print self.experiments[self.comboBox.currentIndex()].experiment.name
+        self.experiment = self.experiments[self.comboBox.currentIndex()]
+        self.tableWidget.setRowCount(len(self.experiments[self.comboBox.currentIndex()].getPlates()))
+        i = 0
+        for  plate in self.experiments[self.comboBox.currentIndex()].getPlates():
+            print plate.name + "  " + plate.platetype3VO.name
+            name = QtGui.QTableWidgetItem(plate.name)
+            sampletype = QtGui.QTableWidgetItem(plate.platetype3VO.name)
+            storageTemperature = QtGui.QTableWidgetItem(plate.storageTemperature)
+            self.tableWidget.setItem(i, 0, name)
+            self.tableWidget.setItem(i, 1, sampletype)
+            self.tableWidget.setItem(i, 2, storageTemperature)
+            i = i + 1
 
 
     def retranslateUi(self, Widget):
@@ -140,7 +140,7 @@ class Ui_Widget(object):
         item.setText(QtGui.QApplication.translate("Widget", "Name", None, QtGui.QApplication.UnicodeUTF8))
         item = self.tableWidget.horizontalHeaderItem(1)
         item.setText(QtGui.QApplication.translate("Widget", "Type", None, QtGui.QApplication.UnicodeUTF8))
-	item = self.tableWidget.horizontalHeaderItem(2)
+        item = self.tableWidget.horizontalHeaderItem(2)
         item.setText(QtGui.QApplication.translate("Widget", "Storage Temp.", None, QtGui.QApplication.UnicodeUTF8))
 
         self.label_2.setText(QtGui.QApplication.translate("Widget", "Select experiment:", None, QtGui.QApplication.UnicodeUTF8))
