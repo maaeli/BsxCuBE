c592c52b62685f8af14dd7f5b69a6eabf6693bad
Staffan <staffan.ohlsson@esrf.fr>
2013-02-19 15:31:28 +0100

    Change from local machine to pydevcz.esrf.fr

--- ControlObjects/BiosaxsClient.py
+++ ControlObjects/BiosaxsClient.py
@@ -6,56 +6,57 @@ import sys
 
 
 
-class BiosaxsClient(CObjectBase):
+class BiosaxsClient( CObjectBase ):
     signals = [
-               Signal("onSuccess"),
-               Signal("onError")
+               Signal( "onSuccess" ),
+               Signal( "onError" )
                ]
     slots = [
-             Slot("getExperimentNamesByProposalCodeNumber"),
-             Slot("setUser"),
-             Slot("getRobotXMLByExperimentId")
+             Slot( "getExperimentNamesByProposalCodeNumber" ),
+             Slot( "setUser" ),
+             Slot( "getRobotXMLByExperimentId" )
              ]
 
-    def init(self):
+    def init( self ):
         self.client = None
-        self.URL = 'http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl'
+        #self.URL = 'http://pcantolinos:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl'
+        self.URL = 'http://pydevcz.esrf.fr:8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl'
         self.selectedExperimentId = None
         self.user = None
         self.password = None
         self.timeout = 5
         self.experiments = None
 
-    def __initWebservice(self):
+    def __initWebservice( self ):
 
-        self.httpAuthenticatedToolsForAutoprocessingWebService = HttpAuthenticated(username = self.user, password = self.password)
-        self.client = Client(self.URL, transport = self.httpAuthenticatedToolsForAutoprocessingWebService, cache = None, timeout = self.timeout)
+        self.httpAuthenticatedToolsForAutoprocessingWebService = HttpAuthenticated( username = self.user, password = self.password )
+        self.client = Client( self.URL, transport = self.httpAuthenticatedToolsForAutoprocessingWebService, cache = None, timeout = self.timeout )
         self.experiments = []
         self.response = None
 
 
-    def setUser(self, user, password):
+    def setUser( self, user, password ):
         self.user = user #"mx1438"
         self.password = password #"Rfo4-73"
 
 
     #Return list containing [["ExperimentName1", "experimentId1"], ["ExperimentName2", "experimentId2"]]
-    def getExperimentNames(self):
+    def getExperimentNames( self ):
 #        print "------------ Getting experiment Names"
         experimentNames = []
         for experiment in self.experiments:
-            experimentNames.append([experiment.experiment.name, experiment.experiment.experimentId])
+            experimentNames.append( [experiment.experiment.name, experiment.experiment.experimentId] )
         return experimentNames
 
-    def getExperimentNamesByProposalCodeNumber(self, code, number):
-        if (self.client is None):
+    def getExperimentNamesByProposalCodeNumber( self, code, number ):
+        if ( self.client is None ):
             self.__initWebservice()
-        response = self.client.service.findExperimentByProposalCode(code, number)
+        response = self.client.service.findExperimentByProposalCode( code, number )
         self.experiments = []
         for experiment in response:
-            self.experiments.append(Experiment(experiment))
+            self.experiments.append( Experiment( experiment ) )
         experimentNames = self.getExperimentNames()
-        self.emit("onSuccess", "getExperimentNamesByProposalCodeNumber", experimentNames)
+        self.emit( "onSuccess", "getExperimentNamesByProposalCodeNumber", experimentNames )
         #self.emit("onSuccess", "getExperimentNamesByProposalCodeNumber", experimentNames)
 
 
@@ -72,8 +73,8 @@ class BiosaxsClient(CObjectBase):
 
 
 
-    def getSpecimenIdBySampleCode(self, sampleCode):
-        print "[ISPyB] getSpecimenIdBySampleCode " + str(sampleCode)
+    def getSpecimenIdBySampleCode( self, sampleCode ):
+        print "[ISPyB] getSpecimenIdBySampleCode " + str( sampleCode )
         if self.experiment is None:
             print "[ISPyB] Experiment is None"
             return None
@@ -90,8 +91,8 @@ class BiosaxsClient(CObjectBase):
         return None
 
     # Return an array of samples which a given concentration
-    def getSpecimensByConcentration(self, concentration):
-        print "[ISPyB] getSpecimenByConcentration: " + str(concentration)
+    def getSpecimensByConcentration( self, concentration ):
+        print "[ISPyB] getSpecimenByConcentration: " + str( concentration )
         if self.experiment is None:
             print "[ISPyB] Experiment is None"
             return None
@@ -103,12 +104,12 @@ class BiosaxsClient(CObjectBase):
         for experiment in self.experiments:
             if experiment.experiment.experimentId == self.selectedExperimentId:
                 for sample in experiment.experiment.samples:
-                    if (float(sample.concentration)) == (float(concentration)):
-                        samples.append(sample)
+                    if ( float( sample.concentration ) ) == ( float( concentration ) ):
+                        samples.append( sample )
         return samples
 
 
-    def getBufferIdByAcronym(self, bufferName):
+    def getBufferIdByAcronym( self, bufferName ):
         for myBuffer in self.experiment.getBuffers():
             if bufferName == myBuffer.acronym:
                 return myBuffer.bufferId
@@ -118,60 +119,60 @@ class BiosaxsClient(CObjectBase):
     # [1] This is a comment
     # ---
     #  |_ Id
-    def getMeasurementIdByCommentId(self, commentId):
+    def getMeasurementIdByCommentId( self, commentId ):
         try:
             for experiment in self.experiments:
                 if experiment.experiment.experimentId == self.selectedExperimentId:
                     for sample in experiment.experiment.samples:
                         for specimen in sample.specimen3VOs:
-                            if str(specimen.comment).find("[" + str(commentId) + "]") != -1:
+                            if str( specimen.comment ).find( "[" + str( commentId ) + "]" ) != -1:
                                 return specimen.specimenId
         except:
              traceback.print_exc()
              raise Exception
         return -1
 
-    def getMeasurementIdBySampleCodeConcentrationAndSEU(self, sampleCode, concentration, seu, bufferName):
+    def getMeasurementIdBySampleCodeConcentrationAndSEU( self, sampleCode, concentration, seu, bufferName ):
         try:
-            print "[ISPyB] getMeasurementIdBySampleCodeConcentrationAndSEU " + str(sampleCode) + " " + str(concentration) + " " + str(seu) + " " + str(bufferName)
+            print "[ISPyB] getMeasurementIdBySampleCodeConcentrationAndSEU " + str( sampleCode ) + " " + str( concentration ) + " " + str( seu ) + " " + str( bufferName )
             if self.experiment is None:
                 print "[ISPyB] Experiment is None"
                 return None
             if self.selectedExperimentId is None:
                 print "[ISPyB] Experiment is None"
                 return None
-            bufferId = self.getBufferIdByAcronym(bufferName)
-            print "[ISPyB] bufferId " + str(bufferId)
+            bufferId = self.getBufferIdByAcronym( bufferName )
+            print "[ISPyB] bufferId " + str( bufferId )
             for experiment in self.experiments:
                 #print experiment.experiment.experimentId
                 if experiment.experiment.experimentId == self.selectedExperimentId:
-                    samples = self.getSpecimensByConcentration(concentration)
+                    samples = self.getSpecimensByConcentration( concentration )
                     for sample in samples:
                         if sample.bufferId == bufferId:
                             for specimen in sample.specimen3VOs:
-                                if (float(specimen.exposureTemperature) == float(seu) and (str(specimen.code) == str(sampleCode))):
+                                if ( float( specimen.exposureTemperature ) == float( seu ) and ( str( specimen.code ) == str( sampleCode ) ) ):
                                     return specimen.specimenId
         except Exception:
              print "[ISPyB] error"
              traceback.print_exc()
              raise Exception
-        print "[ISPyB] Measurement not found with conc: %s SEU: %s and sampleCode:%s" % (str(concentration), str(seu), str(sampleCode))
+        print "[ISPyB] Measurement not found with conc: %s SEU: %s and sampleCode:%s" % ( str( concentration ), str( seu ), str( sampleCode ) )
         return -1
 #Testing git hub
 
     ### Mode: before, after, sample    
-    def saveFrameSet(self, mode, sampleCode, exposureTemperature, storageTemperature, timePerFrame, timeStart, timeEnd, energy, detectorDistance, fileArray, snapshotCapillary, currentMachine, tocollect, pars, specimenId):
+    def saveFrameSet( self, mode, sampleCode, exposureTemperature, storageTemperature, timePerFrame, timeStart, timeEnd, energy, detectorDistance, fileArray, snapshotCapillary, currentMachine, tocollect, pars, specimenId ):
         try:
-            print "[ISPyB] Request for saveFrameSet " + str(mode) + " " + str(sampleCode)
-            if (self.client is None):
+            print "[ISPyB] Request for saveFrameSet " + str( mode ) + " " + str( sampleCode )
+            if ( self.client is None ):
                 self.__initWebservice()
 
             #specimenId = self.getMeasurementIdBySampleCodeConcentrationAndSEU(sampleCode, concentration, ispybSEUtemperature, bufferName)
-            print "[ISPyB] Specimen: " + str(specimenId)
+            print "[ISPyB] Specimen: " + str( specimenId )
             if specimenId is None:
                 specimenId = -1
 
-            self.client.service.saveFrame(mode,
+            self.client.service.saveFrame( mode,
                                           self.selectedExperimentId,
                                           specimenId,
                                           sampleCode,
@@ -185,8 +186,8 @@ class BiosaxsClient(CObjectBase):
                                           fileArray,
                                           snapshotCapillary,
                                           currentMachine,
-                                          str(tocollect),
-                                          str(pars),
+                                          str( tocollect ),
+                                          str( pars ),
                                           pars["beamCenterX"],
                                           pars["beamCenterY"],
                                           pars["radiationRelative"],
@@ -200,7 +201,7 @@ class BiosaxsClient(CObjectBase):
             print "[ISPyB] error", sys.exc_info()[0]
             #traceback.print_exc()
 
-    def getPlatesByPlateGroupId(self, plateGroupId, experimentId):
+    def getPlatesByPlateGroupId( self, plateGroupId, experimentId ):
         plates = []
         for experiment in self.experiments:
             if experiment.experiment.experimentId == experimentId:
@@ -208,23 +209,23 @@ class BiosaxsClient(CObjectBase):
                 for plate in experimentPlates:
                     if plate.plategroup3VO is not None:
                         if plate.plategroup3VO.plateGroupId == plateGroupId:
-                            plates.append(plate)
+                            plates.append( plate )
         return plates
 
-    def getRobotXMLByExperimentId(self, experimentId):
+    def getRobotXMLByExperimentId( self, experimentId ):
         self.selectedExperimentId = experimentId
-        if (self.client is None):
+        if ( self.client is None ):
             self.__initWebservice()
         for experiment in self.experiments:
             plates = []
             if experiment.experiment.experimentId is experimentId:
                 self.selectedExperimentId = experimentId
                 for plate in experiment.getPlates():
-                    plates.append(plate.samplePlateId)
-                return self.client.service.getRobotXMLByPlateIds(experimentId, plates)
+                    plates.append( plate.samplePlateId )
+                return self.client.service.getRobotXMLByPlateIds( experimentId, plates )
         return None
 
-    def getPlateGroupByExperimentId(self, experimentId):
+    def getPlateGroupByExperimentId( self, experimentId ):
         self.selectedExperimentId = experimentId
         for experiment in self.experiments:
             if experiment.experiment.experimentId == experimentId:
@@ -234,21 +235,21 @@ class BiosaxsClient(CObjectBase):
                 groups = experiment.getPlateGroups()
                 names = []
                 for group in groups:
-                    names.append([group.name, group.plateGroupId])
+                    names.append( [group.name, group.plateGroupId] )
                 return names
         return []
 
-    def getRobotXMLByPlateGroupId(self, plateGroupId, experimentId):
-        plates = self.getPlatesByPlateGroupId(plateGroupId, experimentId)
+    def getRobotXMLByPlateGroupId( self, plateGroupId, experimentId ):
+        plates = self.getPlatesByPlateGroupId( plateGroupId, experimentId )
         ids = []
         for plate in plates:
-            ids.append(plate.samplePlateId)
-        xml = self.client.service.getRobotXMLByPlateIds(experimentId, str(ids))
-        self.emit("onSuccess", "getRobotXMLByPlateGroupId", xml)
+            ids.append( plate.samplePlateId )
+        xml = self.client.service.getRobotXMLByPlateIds( experimentId, str( ids ) )
+        self.emit( "onSuccess", "getRobotXMLByPlateGroupId", xml )
 
-    def createExperiment(self, proposalCode, proposalNumber, samples, storageTemperature, mode, extraflowTime):
+    def createExperiment( self, proposalCode, proposalNumber, samples, storageTemperature, mode, extraflowTime ):
         try:
-            if (self.client is None):
+            if ( self.client is None ):
                 self.__initWebservice()
         except Exception:
             print "[ISPyB] It has been not possible to connect with ISPyB. No connection"
@@ -256,17 +257,17 @@ class BiosaxsClient(CObjectBase):
         try:
             self.experiment = None
             self.selectedExperimentId = None
-            print "[ISPyB] Request to ISPyB: create new experiment for proposal " + str(proposalCode) + str(proposalNumber)
-            experiment = self.client.service.createExperiment(proposalCode, proposalNumber, str(samples), storageTemperature, mode, extraflowTime)
-            if (experiment is None):
+            print "[ISPyB] Request to ISPyB: create new experiment for proposal " + str( proposalCode ) + str( proposalNumber )
+            experiment = self.client.service.createExperiment( proposalCode, proposalNumber, str( samples ), storageTemperature, mode, extraflowTime )
+            if ( experiment is None ):
                 print "[ISPyB] ISPyB could not create the experiment from robot file"
                 raise Exception
 
-            print "[ISPyB] Experiment Created: " + str(experiment.experimentId)
+            print "[ISPyB] Experiment Created: " + str( experiment.experimentId )
             self.selectedExperimentId = experiment.experimentId
-            self.experiment = Experiment(experiment)
-            self.experiments.append(Experiment(experiment))
-            print "[ISPyB] selectedExperimentId: " + str(self.selectedExperimentId)
+            self.experiment = Experiment( experiment )
+            self.experiments.append( Experiment( experiment ) )
+            print "[ISPyB] selectedExperimentId: " + str( self.selectedExperimentId )
             #print experiment
         except Exception:
             print "[ISPyB] handled error"
@@ -275,35 +276,35 @@ class BiosaxsClient(CObjectBase):
             #traceback.print_exc()
 
 class Experiment:
-    def __init__(self, experiment):
+    def __init__( self, experiment ):
         self.experiment = experiment
 
-    def getBuffers(self):
+    def getBuffers( self ):
         return self.experiment.buffer3VOs
 
-    def getPlates(self):
+    def getPlates( self ):
         return self.experiment.samplePlate3VOs
 
-    def getPlateGroups(self):
+    def getPlateGroups( self ):
         plates = self.getPlates()
         print plates
         dict = {}
         plateGroups = []
         for plate in plates:
-            print str(plate)
-            if hasattr(plate, 'plategroup3VO'):
+            print str( plate )
+            if hasattr( plate, 'plategroup3VO' ):
                 print "contiene plategroup3VO"
-                if not dict.has_key(plate.plategroup3VO.name):
+                if not dict.has_key( plate.plategroup3VO.name ):
                     print "plate.plategroup3VO.name"
                     if plate.plategroup3VO is not None:
-                        plateGroups.append(plate.plategroup3VO)
+                        plateGroups.append( plate.plategroup3VO )
                     dict[plate.plategroup3VO.name] = True
         return plateGroups
 
 if __name__ == "__main__":
-    biosaxs = BiosaxsClient("mx1438", "Rfo4-73")
-    biosaxs.getExperimentNamesByProposalCodeNumber("mx", "1438")
+    biosaxs = BiosaxsClient( "mx1438", "Rfo4-73" )
+    biosaxs.getExperimentNamesByProposalCodeNumber( "mx", "1438" )
     #experiment = biosaxs.getExperimentById(345)
     biosaxs.selectedExperimentId = 345
-    print biosaxs.getSpecimenIdBySampleCode("bsa_25C")
+    print biosaxs.getSpecimenIdBySampleCode( "bsa_25C" )
 
