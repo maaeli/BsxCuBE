d5c3dbaedb177e6edec2f745c94a956e6c03ba7f
bliss administrator <blissadm@oreillici.esrf.fr>
2018-07-05 10:32:48 +0200

    Fixed bug which shekced rad. damages settings when taking a test image

--- ControlObjects/Collect.py
+++ ControlObjects/Collect.py
@@ -368,10 +368,11 @@ class Collect( CObjectBase ):
         self.collectBeamCenterX.set_value( pBeamCenterX )
         self.collectBeamCenterY.set_value( pBeamCenterY )
         self.collectNormalisation.set_value( pNormalisation )
-        self.collectRadiationDamage.set_value( pRadiationChecked )
+        
+       #self.collectRadiationDamage.set_value( pRadiationChecked )
        
-        self.collectRelativeRadiationDamage.set_value( pRadiationRelative )
-        self.collectAbsoluteRadiationDamage.set_value( pRadiationAbsolute )
+        #self.collectRelativeRadiationDamage.set_value( pRadiationRelative )
+        #self.collectAbsoluteRadiationDamage.set_value( pRadiationAbsolute )
         # note that pTimePerFrame is 1.0 s and number of frames is 1
         #pNumberFrames = 1
         #pTimePerFrame = 1.0
